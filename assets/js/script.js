1ca23fb286dee19b673928c5d408f5b0813e1ec1
Bosss <masdadan322@gmail.com>
2026-09-20 11:53:28 +0700

    add edu, js, foto

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+const menuToggle = document.getElementById("menuToggle");
+const navLinks = document.getElementById("navLinks");
+
+menuToggle.addEventListener("click", () => {
+    const isOpen = navLinks.classList.toggle("active");
+
+    menuToggle.setAttribute(
+        "aria-expanded", isOpen);
+});
